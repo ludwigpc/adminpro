35579d46db06ab78c1cbd51e333cb5d079f8cafc
Ludwig PC <ludwigpc@live.com>
2019-05-20 16:38:53 -0500

    Progress page funcionando

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from "@angular/core";
 import { PAGES_ROUTES } from "./pages.routes";
 
+import { SharedModule } from "../shared/shared.module";
+
+import { FormsModule } from '@angular/forms';
+
+import { PagesComponent } from "./pages.component";
+
+//import { ChartsModule } from 'ng2-charts';
+
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { ProgressComponent } from "./progress/progress.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
-import { PagesComponent } from "./pages.component";
-import { SharedModule } from "../shared/shared.module";
+
+
+import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
 
 
 @NgModule({
@@ -13,17 +22,22 @@ import { SharedModule } from "../shared/shared.module";
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        PagesComponent
+        PagesComponent,
+         //temporal
+         IncrementadorComponent
     ], 
     exports: [
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        PagesComponent
+
     ],
     imports:[
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+      //  ChartsModule
+       
     ]
 })
 
